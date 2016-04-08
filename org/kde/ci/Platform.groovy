d909890c35983f8470a257fbcb915e5592fdb5fd
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2016-04-07 17:56:24 -0700

    tracks test

--- org/kde/ci/Platform.groovy
+++ org/kde/ci/Platform.groovy
@@ -56,7 +56,7 @@ class Platform {
 	
 	def genCurrentPlatform(options, track) {
 		def tracks = options.find { key, value -> key == 'tracks' }
-			 if( tracks.contains(track.toString())) { return true }	
+			 if( tracks.toString() == track ) { return true }	
 	}
 	static Closure PlatformVariations(Variations) {
 		
