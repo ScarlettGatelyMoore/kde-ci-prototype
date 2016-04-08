ecb09d2c4698d5d023ea3d80a248d44318d93a3f
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2016-04-07 17:54:04 -0700

    convert tracks to string

--- org/kde/ci/Platform.groovy
+++ org/kde/ci/Platform.groovy
@@ -56,7 +56,7 @@ class Platform {
 	
 	def genCurrentPlatform(options, track) {
 		def tracks = options.find { key, value -> key == 'tracks' }
-			 if( tracks.contains(track)) { return true }	
+			 if( tracks.contains(track.toString())) { return true }	
 	}
 	static Closure PlatformVariations(Variations) {
 		
