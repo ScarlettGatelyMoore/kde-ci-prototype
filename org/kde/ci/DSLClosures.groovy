32290b07c3666ab82b6d40e91efac17df42b30da
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2016-04-08 07:59:09 -0700

    shell return

--- org/kde/ci/DSLClosures.groovy
+++ org/kde/ci/DSLClosures.groovy
@@ -81,12 +81,8 @@ class DSLClosures {
 		} else {
 			shell = 'shell'
 		}
-		return { project ->
-			project / "${shell}"('python3 '+ "${home}" + '/scripts/tools/update-setup-sandbox-local.py' + "\n" + \
-						  'python '+ "${home}" + '/scripts/tools/prepare-environment.py' + "\n" + \
-						  'python '+ "${home}" + '/scripts/tools/perform-build.py'
+		return "${shell}"('python3 '+ "${home}" + '/scripts/tools/update-setup-sandbox-local.py' + "\n" + 'python '+ "${home}" + '/scripts/tools/prepare-environment.py' + "\n" + 'python '+ "${home}" + '/scripts/tools/perform-build.py'
 						)
-		}
 	}
 
 }
