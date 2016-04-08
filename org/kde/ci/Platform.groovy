b602776fe44715d6eee76b61d4738dd167fd731c
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2016-04-07 19:33:55 -0700

    remove extra print

--- org/kde/ci/Platform.groovy
+++ org/kde/ci/Platform.groovy
@@ -46,8 +46,7 @@ class Platform {
 	}
 	def genCompilers(options) {
 		def compiler = options.find { key, value -> key == 'compilers' }.getValue()	
-		def num = compiler.size()
-		return compiler.size()
+		def num = compiler.size()		
 		if ( num > 1 ) {
 			List compilers = compiler	
 			return compilers 
