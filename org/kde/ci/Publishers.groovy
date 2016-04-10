6c657dfaacd28737c18f4dbbe6bed404dd9259d0
Scarlett Clark <scarlett@scarlettgatelyclark.com>
2016-04-09 20:49:46 -0700

    flexible

--- org/kde/ci/Publishers.groovy
+++ org/kde/ci/Publishers.groovy
@@ -40,7 +40,7 @@ class Publishers {
 	}
 	def genWarningsPublisher(platform, compiler) {
 		return	{ node ->
-				  node / 'publishers' / 'org.jenkins__ci.plugins.flexible__publish.ConditionalPublisher' {
+				  node / 'FlexiblePublisher' / 'publishers' / 'org.jenkins__ci.plugins.flexible__publish.ConditionalPublisher' {
 					condition(class: 'org.jenkins_ci.plugins.run_condition.core.StringsMatchCondition') {
 						arg1 '${ENV,var="compiler"}'
 						arg2 "${compiler}"
