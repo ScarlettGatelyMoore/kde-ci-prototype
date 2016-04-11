732e86131bb552856fc74cd1945f8dfed89bef9a
Scarlett Clark <sgclark@kde.org>
2016-04-11 12:52:14 -0700

    fix regex

--- org/kde/ci/ImportConfig.groovy
+++ org/kde/ci/ImportConfig.groovy
@@ -51,7 +51,7 @@ class ImportConfig {
 		dir.eachFileRecurse (FileType.FILES) { file ->
 			repofiles << file
 		}
-		repofiles.removeAll(~/i18n/)
+		repofiles.removeAll { it =~ /i18n/ }
 		return repofiles
 	}
 
