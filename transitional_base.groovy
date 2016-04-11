9a0f1fafe4e63c886ca43273aa27cfc7e42a70b6
Scarlett Clark <sgclark@kde.org>
2016-04-11 14:01:22 -0700

    print repopath

--- transitional_base.groovy
+++ transitional_base.groovy
@@ -39,7 +39,7 @@ repoDataFile.each { file ->
 	def project = file.toString().minus(repobasePath).minus('/metadata.yaml')
 	def projrepoyaml = configs.getConfig(file)
 	RepoMetaValues rd = RepoMetaValues.newInstance(projrepoyaml)
-	println rd
+	println rd.repopath
 }
 
 println repoConfig
