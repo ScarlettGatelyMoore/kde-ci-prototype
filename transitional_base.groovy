6af26722e89467065ab56c239de24dc7f57f638d
Scarlett Clark <sgclark@kde.org>
2016-04-11 15:21:01 -0700

    regex

--- transitional_base.groovy
+++ transitional_base.groovy
@@ -41,7 +41,7 @@ repoDataFile.each { file ->
 	project = projectpart =~ /\s(\w+)$/	
 	def projrepoyaml = configs.getConfig(file)
 	RepoMetaValues rd = RepoMetaValues.newInstance(projrepoyaml)
-	println project
+	println project[0]
 	println rd.projectpath
 }
 
