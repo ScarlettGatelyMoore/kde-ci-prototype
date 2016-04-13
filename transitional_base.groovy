38bf6405a7b00f14794e0e5252ba9eb4f9b7d623
Scarlett Clark <sgclark@kde.org>
2016-04-13 15:01:42 -0700

    Look for wildcard or full path

--- transitional_base.groovy
+++ transitional_base.groovy
@@ -87,7 +87,7 @@ GroupFile.each { group ->
 			assert job.getActive() == true
 			// Bring in development tracks to determine branches.
 			Map repository = job.SetRepoMap()
-			def tracks = repository.getAt('branches')
+			def tracks = repository.findAll(groupName + '/*' || path )
 			// We have branchGroups that split into sections for releases/development 
 			// We need to process a new jobset for each of these groups.
 			Map bg = job.getBranchGrouptracks()
