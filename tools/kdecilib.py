25596a39cd06ff490bd7671b0c91b40e0f8d1b88
Scarlett Clark <sgclark@kde.org>
2016-06-16 02:49:59 -0700

    disable more failed py3 convert.

--- tools/kdecilib.py
+++ tools/kdecilib.py
@@ -986,9 +986,9 @@ class BuildManager(object):
 			else:
 				command = self.config.get('Test', 'dbusLaunchCommand')
 				process = check_output( shlex.split(command), env=runtimeEnv )
-				for variable in process:
-					splitVars = variable.split('=', 1)
-					runtimeEnv[ splitVars[0] ] = splitVars[1].strip()			
+# 				for variable in process:
+# 					splitVars = variable.split('=', 1)
+# 					runtimeEnv[ splitVars[0] ] = splitVars[1].strip()			
 
 		# Rebuild the Sycoca
 		command = self.config.get('Test', 'kbuildsycocaCommand')
