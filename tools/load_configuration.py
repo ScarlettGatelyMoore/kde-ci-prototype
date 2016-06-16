3aa468c9867ee7eecb822fb72ae37b6a874d891d
Scarlett Clark <sgclark@kde.org>
2016-06-15 23:23:00 -0700

    Fix spaces in regex

--- tools/load_configuration.py
+++ tools/load_configuration.py
@@ -13,7 +13,7 @@ def check_jenkins_environment():
 	# Do we have a job name?
 	if 'JOB_NAME' in os.environ:
 		# Split it out
-		jobMatch = re.match("(?P<project>[^ ]+) ?(?P<branchGroup>[^_]+)? ?(?P<track>[^_/]+)? ?(?P<branch>[^_/]+)? ?(?P<platform>[^_/]+)? ?(?P<compiler>[^_/]+)?", os.environ['JOB_NAME'])
+		jobMatch = re.match("(?P<project>[^ ]+) ?(?P<branchGroup>[^ ]+)? ?(?P<track>[^ ]+)? ?(?P<branch>[^ ]+)? ?(?P<platform>[^ ]+)? ?(?P<compiler>[^ ])?", os.environ['JOB_NAME'])
 		# Now transfer in any non-None attributes
 		# If we have the project name, transfer it
 		if jobMatch.group('project') is not None:
